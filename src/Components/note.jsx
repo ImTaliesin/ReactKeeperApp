67651f8956ee4deb71fa125dfb9d6d7df9b3102e
ImTaliesin <brennanjdouglas@gmail.com>
2023-07-27 23:13:58 -0700

    Template for the notes app

---
+++ src/Components/note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+        <h1>This is the title of a note</h1>
+        <p>This is the content of a note</p>
+        </div>
+    );
+}
+
+export default Note;
